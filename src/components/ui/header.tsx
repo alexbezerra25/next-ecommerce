7868af093c755748e43cffa086f470e99baa33d3
Alex <14alex25@gmail.com>
2023-10-21 23:18:42 -0300

    chore: setup next auth

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,21 +1,49 @@
-import { MenuIcon, ShoppingCartIcon } from 'lucide-react'
-import { Card } from './card'
-import { Button } from './button'
+import { HomeIcon, ListOrderedIcon, LogInIcon, MenuIcon, PercentIcon, ShoppingCartIcon } from "lucide-react";
+import { Card } from "./card";
+import { Button } from "./button";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 
 const Header = () => {
   return (
-    <Card className='flex justify-between items-center p-[1.875rem]'>
-        <Button size='icon' variant='outline'>
-            <MenuIcon/>
-        </Button>
+    <Card className="flex items-center justify-between p-[1.875rem]">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button size="icon" variant="outline">
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
 
-        <h1 className='font-semibold text-lg'><span className='text-primary'>Next</span> E-commerce</h1>
+        <SheetContent side="left">
+          <SheetHeader className="text-left text-lg font-semibold">
+            Menu
+          </SheetHeader>
 
-        <Button size='icon' variant='outline'>
-            <ShoppingCartIcon />
-        </Button>
+          <div className="mt-2 flex flex-col gap-2">
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <LogInIcon size={16}/> Fazer login
+            </Button>
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <HomeIcon size={16}/> Início
+            </Button>
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <PercentIcon size={16}/> Ofertas
+            </Button>
+            <Button variant="outline" className="w-full justify-start gap-2">
+              <ListOrderedIcon size={16}/> Catálogo
+            </Button>
+          </div>
+        </SheetContent>
+      </Sheet>
+
+      <h1 className="text-lg font-semibold">
+        <span className="text-primary">Next</span> E-commerce
+      </h1>
+
+      <Button size="icon" variant="outline">
+        <ShoppingCartIcon />
+      </Button>
     </Card>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
