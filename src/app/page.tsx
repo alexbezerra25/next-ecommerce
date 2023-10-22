e2aeccf7dcfff2052d3c6d6951e680030119dbf7
Alex <14alex25@gmail.com>
2023-10-22 00:01:05 -0300

    feat: add authentication user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+'use client';
+
 import { Card } from '@/components/ui/card'
+import { useSession } from 'next-auth/react';
 import Image from 'next/image'
 
 export default function Home() {
+  const {data} = useSession();
   return (
     <div>
       <Card>
-        Teste
+        <h1>{data?.user?.name}</h1>
       </Card>
     </div>
   )
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,9 +1,29 @@
-import { HomeIcon, ListOrderedIcon, LogInIcon, MenuIcon, PercentIcon, ShoppingCartIcon } from "lucide-react";
+"use client";
+import {
+  HomeIcon,
+  ListOrderedIcon,
+  LogInIcon,
+  LogOutIcon,
+  MenuIcon,
+  PercentIcon,
+  ShoppingCartIcon,
+} from "lucide-react";
 import { Card } from "./card";
 import { Button } from "./button";
 import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+import { Separator } from "./separator";
 
 const Header = () => {
+  const handleLoginClick = async () => {
+    await signIn();
+  };
+  const handleLogoutClick = async () => {
+    await signOut();
+  };
+
+  const { status, data } = useSession();
   return (
     <Card className="flex items-center justify-between p-[1.875rem]">
       <Sheet>
@@ -18,18 +38,52 @@ const Header = () => {
             Menu
           </SheetHeader>
 
-          <div className="mt-2 flex flex-col gap-2">
+
+          {status === 'authenticated' && data?.user && (
+          <div className="my-3 flex items-center gap-2">
+            <Avatar>
+              <AvatarFallback>
+                {data.user.name?.[0].toUpperCase()}
+              </AvatarFallback>
+
+              {data.user.image && <AvatarImage src={data.user.image} />}
+            </Avatar>
+
+            <p className="font-medium">{data.user.name}</p>
+          </div>
+          )}
+
+          <Separator/>
+
+          <div className="mt-3 flex flex-col gap-2">
+            {status === 'unauthenticated' && (
+              <Button
+              onClick={handleLoginClick}
+              variant="outline"
+              className="w-full justify-start gap-2"
+            >
+              <LogInIcon size={16} /> Fazer login
+            </Button>
+            )}
+
+            {status === 'authenticated' && data.user && (
+              <Button
+              onClick={handleLogoutClick}
+              variant="outline"
+              className="w-full justify-start gap-2"
+            >
+              <LogOutIcon size={16} /> Sair
+            </Button>
+            )}
+            
             <Button variant="outline" className="w-full justify-start gap-2">
-              <LogInIcon size={16}/> Fazer login
+              <HomeIcon size={16} /> Início
             </Button>
             <Button variant="outline" className="w-full justify-start gap-2">
-              <HomeIcon size={16}/> Início
+              <PercentIcon size={16} /> Ofertas
             </Button>
             <Button variant="outline" className="w-full justify-start gap-2">
-              <PercentIcon size={16}/> Ofertas
-            </Button>
-            <Button variant="outline" className="w-full justify-start gap-2">
-              <ListOrderedIcon size={16}/> Catálogo
+              <ListOrderedIcon size={16} /> Catálogo
             </Button>
           </div>
         </SheetContent>
