5b6ea9d809fe917342f9f213ebd68a90375d3e4f
Alex <14alex25@gmail.com>
2023-10-22 21:15:10 -0300

    feat: add increase and decrease product of cart

--- src/components/ui/cart-item.tsx
+++ src/components/ui/cart-item.tsx
@@ -1,13 +1,17 @@
-import { CartProduct } from "@/providers/cart";
+"use client";
+import { CartContext, CartProduct } from "@/providers/cart";
 import { ArrowLeftIcon, ArrowRightIcon, TrashIcon } from "lucide-react";
 import Image from "next/image";
 import { Button } from "./button";
+import { useContext } from "react";
 
 interface CartItemProps {
   product: CartProduct;
 }
 
 const CartItem = ({ product }: CartItemProps) => {
+    const {increaseProductQuantity, decreaseProductQuantity} = useContext(CartContext)
+
   return (
     <div className="flex items-center justify-between">
       {/* Parte esquerda */}
@@ -41,13 +45,13 @@ const CartItem = ({ product }: CartItemProps) => {
           </div>
 
           <div className="mt-1 flex items-center gap-2">
-            <Button variant={"outline"} size={"icon"} className="h-8 w-8">
+            <Button variant={"outline"} size={"icon"} className="h-8 w-8" onClick={()=>{decreaseProductQuantity(product.id)}}>
               <ArrowLeftIcon size={16} />
             </Button>
 
             <span className="text-sm">{product.quantity}</span>
 
-            <Button variant={"outline"} size={"icon"} className="h-8 w-8">
+            <Button variant={"outline"} size={"icon"} className="h-8 w-8" onClick={()=>{increaseProductQuantity(product.id)}}>
               <ArrowRightIcon size={16} />
             </Button>
           </div>
--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -7,12 +7,14 @@ export interface CartProduct extends ProductWithTotalPrice {
 }
 
 interface ICartContext {
-    products: CartProduct[];
-    cartTotalPrice: number;
-    cartBasePrice: number;
-    cartTotalDiscount: number;
-    addProductToCart: (product: CartProduct) => void;
-  }
+  products: CartProduct[];
+  cartTotalPrice: number;
+  cartBasePrice: number;
+  cartTotalDiscount: number;
+  addProductToCart: (product: CartProduct) => void;
+  decreaseProductQuantity: (productId: string) => void;
+  increaseProductQuantity: (productId: string) => void;
+}
 
 export const CartContext = createContext<ICartContext>({
   products: [],
@@ -20,41 +22,77 @@ export const CartContext = createContext<ICartContext>({
   cartBasePrice: 0,
   cartTotalDiscount: 0,
   addProductToCart: () => {},
+  decreaseProductQuantity: () => {},
+  increaseProductQuantity: () => {},
 });
 
+const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [products, setProducts] = useState<CartProduct[]>([]);
 
-const CartProvider = ({ children }: {children: ReactNode}) => {
-    const [products, setProducts] = useState<CartProduct[]>([])
+  const addProductToCart = (product: CartProduct) => {
+    // se o produto já existir no carrinho, então incrementar a quantidade
+    const productIsAlreadyOnCart = products.some(
+      (cartProduct) => cartProduct.id === product.id,
+    );
 
-    const addProductToCart = (product: CartProduct)=>{
-        // se o produto já existir no carrinho, então incrementar a quantidade
-        const productIsAlreadyOnCart = products.some((cartProduct)=> cartProduct.id === product.id)
-
-        if(productIsAlreadyOnCart){
-            setProducts((prev)=>
-                prev.map((cartProduct)=>{
-                    if(cartProduct.id === product.id){
-                        return {
-                            ...cartProduct,
-                            quantity: cartProduct.quantity + product.quantity
-                        }
-                    }
-                    return cartProduct;
-                })
-            )
-            return;
-        }
-        // se não, adicione à lista
-        setProducts((prev)=>[...prev, product])
+    if (productIsAlreadyOnCart) {
+      setProducts((prev) =>
+        prev.map((cartProduct) => {
+          if (cartProduct.id === product.id) {
+            return {
+              ...cartProduct,
+              quantity: cartProduct.quantity + product.quantity,
+            };
+          }
+          return cartProduct;
+        }),
+      );
+      return;
     }
+    // se não, adicione à lista
+    setProducts((prev) => [...prev, product]);
+  };
+
+  const decreaseProductQuantity = (productId: string) => {
+    setProducts((prev) =>
+      prev
+        .map((cartProduct) => {
+          if (cartProduct.id === productId) {
+            return {
+              ...cartProduct,
+              quantity: cartProduct.quantity - 1,
+            };
+          }
+          return cartProduct;
+        })
+        .filter((cartProduct) => cartProduct.quantity > 0),
+    );
+  };
+
+  const increaseProductQuantity = (productId: string) => {
+    setProducts((prev) =>
+      prev.map((cartProduct) => {
+        if (cartProduct.id === productId) {
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity + 1,
+          };
+        }
+        return cartProduct;
+      }),
+    );
+  };
+
   return (
     <CartContext.Provider
       value={{
         products,
-        addProductToCart,
         cartTotalPrice: 0,
         cartBasePrice: 0,
         cartTotalDiscount: 0,
+        addProductToCart,
+        decreaseProductQuantity,
+        increaseProductQuantity,
       }}
     >
       {children}
