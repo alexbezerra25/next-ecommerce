0e2e5d74e09a077ce394ba3c52daff28f66a9fac
Alex <14alex25@gmail.com>
2023-10-28 23:15:05 -0300

    feat: add toggle theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/ui/header";
 import { AuthProvider } from "@/providers/auth";
 import Footer from "@/components/ui/footer";
 import CartProvider from "@/providers/cart";
+import { ThemeProvider } from "@/providers/theme"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,13 @@ export default function RootLayout({
     <html lang="pt-br">
       <body className={inter.className}>
         <div className="flex h-full flex-col">
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+
           <AuthProvider>
             <CartProvider>
               <Header />
@@ -29,6 +37,8 @@ export default function RootLayout({
               <Footer />
             </CartProvider>
           </AuthProvider>
+
+          </ThemeProvider>
         </div>
       </body>
     </html>
--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,118 +1,126 @@
 import {
-    HomeIcon,
-    ListOrderedIcon,
-    LogInIcon,
-    LogOutIcon,
-    MenuIcon,
-    PercentIcon,
-  } from "lucide-react";
+  HomeIcon,
+  ListOrderedIcon,
+  LogInIcon,
+  LogOutIcon,
+  MenuIcon,
+  PackageSearchIcon,
+  PercentIcon,
+} from "lucide-react";
 import { Button } from "./button";
 import {
-    Sheet,
-    SheetClose,
-    SheetContent,
-    SheetHeader,
-    SheetTrigger,
-  } from "./sheet";
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from "./sheet";
 
 import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
 import { Separator } from "./separator";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { ModeToggle } from "./toggle-theme";
 
 const Sidebar = () => {
+  const handleLoginClick = async () => {
+    await signIn();
+  };
+  const handleLogoutClick = async () => {
+    await signOut();
+  };
 
-    const handleLoginClick = async () => {
-        await signIn();
-      };
-      const handleLogoutClick = async () => {
-        await signOut();
-      };
-    
-      const { status, data } = useSession();
-    return ( 
-        <Sheet>
-        <SheetTrigger asChild>
-          <Button size="icon" variant="outline">
-            <MenuIcon />
-          </Button>
-        </SheetTrigger>
+  const { status, data } = useSession();
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button size="icon" variant="outline">
+          <MenuIcon />
+        </Button>
+      </SheetTrigger>
 
-        <SheetContent side="left">
-          <SheetHeader className="text-left text-lg font-semibold">
-            Menu
-          </SheetHeader>
+      <SheetContent side="left" className="h-full flex flex-col">
+        <SheetHeader className="text-left text-lg font-semibold">
+          Menu
+        </SheetHeader>
 
-          {status === "authenticated" && data?.user && (
-            <div className="my-3 flex items-center gap-2">
-              <Avatar>
-                <AvatarFallback>
-                  {data.user.name?.[0].toUpperCase()}
-                </AvatarFallback>
+        {status === "authenticated" && data?.user && (
+          <div className="my-3 flex items-center gap-2">
+            <Avatar>
+              <AvatarFallback>
+                {data.user.name?.[0].toUpperCase()}
+              </AvatarFallback>
 
-                {data.user.image && <AvatarImage src={data.user.image} />}
-              </Avatar>
+              {data.user.image && <AvatarImage src={data.user.image} />}
+            </Avatar>
 
-              <p className="font-medium">{data.user.name}</p>
-            </div>
+            <p className="font-medium">{data.user.name}</p>
+          </div>
+        )}
+
+        <Separator />
+
+        <div className="mt-3 flex flex-col gap-2">
+          {status === "unauthenticated" && (
+            <Button
+              onClick={handleLoginClick}
+              variant="outline"
+              className="w-full justify-start gap-2"
+            >
+              <LogInIcon size={16} /> Fazer login
+            </Button>
           )}
 
-          <Separator />
+          {status === "authenticated" && data.user && (
+            <Button
+              onClick={handleLogoutClick}
+              variant="outline"
+              className="w-full justify-start gap-2"
+            >
+              <LogOutIcon size={16} /> Sair
+            </Button>
+          )}
 
-          <div className="mt-3 flex flex-col gap-2">
-            {status === "unauthenticated" && (
-              <Button
-                onClick={handleLoginClick}
-                variant="outline"
-                className="w-full justify-start gap-2"
-              >
-                <LogInIcon size={16} /> Fazer login
+          <SheetClose asChild>
+            <Link href="/">
+              <Button variant="outline" className="w-full justify-start gap-2">
+                <HomeIcon size={16} /> Início
               </Button>
-            )}
+            </Link>
+          </SheetClose>
 
-            {status === "authenticated" && data.user && (
-              <Button
-                onClick={handleLogoutClick}
-                variant="outline"
-                className="w-full justify-start gap-2"
-              >
-                <LogOutIcon size={16} /> Sair
+          <SheetClose asChild>
+            <Link href="/orders">
+              <Button variant="outline" className="w-full justify-start gap-2">
+                <PackageSearchIcon size={16} />
+                Meus Pedidos
               </Button>
-            )}
+            </Link>
+          </SheetClose>
 
-            <SheetClose asChild>
-              <Link href="/">
-                <Button
-                  variant="outline"
-                  className="w-full justify-start gap-2"
-                >
-                  <HomeIcon size={16} /> Início
-                </Button>
-              </Link>
-            </SheetClose>
-
-            <SheetClose asChild>
+          <SheetClose asChild>
             <Link href="/deals">
               <Button variant="outline" className="w-full justify-start gap-2">
                 <PercentIcon size={16} /> Ofertas
               </Button>
-              </Link>
-            </SheetClose>
+            </Link>
+          </SheetClose>
 
-            <SheetClose asChild>
-              <Link href="/catalogo">
-                <Button
-                  variant="outline"
-                  className="w-full justify-start gap-2"
-                >
-                  <ListOrderedIcon size={16} /> Catálogo
-                </Button>
-              </Link>
-            </SheetClose>
-          </div>
-        </SheetContent>
-      </Sheet>
-     );
-}
- 
+          <SheetClose asChild>
+            <Link href="/catalogo">
+              <Button variant="outline" className="w-full justify-start gap-2">
+                <ListOrderedIcon size={16} /> Catálogo
+              </Button>
+            </Link>
+          </SheetClose>
+        </div>
+
+        <div className="mt-auto">
+          <ModeToggle />
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
 export default Sidebar;
